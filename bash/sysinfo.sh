60b7dd7242b74b0b7e8ff3e67fec441fff4588a6
Gurpartap <200509650@student.georgianc.on.ca>
2023-02-15 18:15:50 -0500

    My second script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-echo "The fully qualified domain name for system is:"
-hostname
-echo "The information for host is:"
-hostnamectl
-echo "The IP address of this server is:"
-hostname -I
-echo "Available amount of storage in root system:"
-df -h /
+echo "My second script"
+#dat1 is the variable which will provide the hostname
+dat1=$(hostname)
+#dat2 is a variable which will provide the fully qualified name of the domain
+dat2=$(hostname -f)
+#dat3 will provide the whole information regarding the system and grep will trim its operating system information
+dat3=$(hostnamectl | grep Operating) 
+#dat4 is the variable which will provide the host name IP address
+dat4=$(hostname -I)
+#dat5 is a variable which will give us the system information in which awk is used to provide only the fourth line and then tail is used here because we only want the last line 
+dat5=$(df -h / | awk '{print $4}' | tail -n 1)
+cat <<EOF
+Report for:$dat1
+===============
+FQDN:$dat2
+Name and version of operating system:$dat3
+IP address:$dat4
+Free Space In Root File System:$dat5
+===============
+EOF
